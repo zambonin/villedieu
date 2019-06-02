f580fd864666e979d89f7b927522273b73615ab7
Gustavo Zambonin <gzmbnn@gmail.com>
2019-06-02 15:51:20 -0300

    dijkstra: style and type hint corrections

--- optimization/cheap_route.py
+++ optimization/cheap_route.py
@@ -1,38 +1,42 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+# pylint: disable=C0330, R0913
 
+from __future__ import absolute_import, division
 from typing import List, Tuple
 
 
-def dijkstra(graph: List[List[float]], source: int) -> Tuple[List[int]]:
+def dijkstra(
+    graph: List[List[float]], source: int
+) -> Tuple[List[float], List[int]]:
 
     dist = [float("inf")] * len(graph)
     dist[source] = 0
 
-    prev = [None] * len(graph)
+    prev = [-1] * len(graph)
     vertices = set(range(len(graph)))
 
     while vertices:
         curr = min(vertices, key=lambda v: dist[v])
         vertices.remove(curr)
 
-        for v in set(graph[curr].index(e) for e in graph[curr] if e):
-            alt = dist[curr] + graph[curr][v]
-            if alt < dist[v]:
-                dist[v] = alt
-                prev[v] = curr
+        for vert in set(graph[curr].index(e) for e in graph[curr] if e):
+            alt = dist[curr] + graph[curr][vert]
+            if alt < dist[vert]:
+                dist[vert] = alt
+                prev[vert] = curr
 
     return dist, prev
 
 
-def q1(
+def cheapest_route(
     graph: List[List[int]],
     tolls: List[float],
     source: int,
     dest: int,
     gas_price: float,
     autonomy: float,
-) -> List[int]:
+) -> Tuple[List[int], float]:
 
     assert len(graph) == len(graph[0]) == len(tolls)
     assert source <= len(graph) and dest <= len(graph)
@@ -45,7 +49,7 @@ def q1(
     dist, prev = dijkstra(mod_graph, source)
     path, curr = [], dest
 
-    while prev[curr] is not None:
+    while prev[curr] >= 0:
         path.append(curr)
         curr = prev[curr]
     path.append(curr)
@@ -54,9 +58,9 @@ def q1(
     return path, dist[dest]
 
 
-if __name__ == "__main__":
+def known_answer():
     # http://web.stanford.edu/class/archive/cs/cs106b/cs106b.1152/images/graph-dijkstra-figure-1.png
-    g = [
+    graph = [
         [0, 4, 0, 2, 7, 0, 0, 0],
         [0, 0, 0, 0, 2, 0, 0, 0],
         [0, 0, 0, 0, 4, 0, 0, 0],
@@ -67,10 +71,17 @@ if __name__ == "__main__":
         [0, 0, 0, 0, 5, 1, 0, 0],
     ]
 
-    p = [1, 2, 3, 4, 5, 6, 7, 8]
-    s = 0
-    d = 5
-    gp = 2
-    a = 5
+    tolls = [1, 2, 3, 4, 5, 6, 7, 8]
+    source = 0
+    dest = 5
+    gas_price = 2
+    autonomy = 5
 
-    print(q1(g, p, s, d, gp, a))
+    assert cheapest_route(graph, tolls, source, dest, gas_price, autonomy) == (
+        [0, 4, 5],
+        9.6,
+    )
+
+
+if __name__ == "__main__":
+    known_answer()
